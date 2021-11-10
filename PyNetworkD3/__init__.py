60b09a6c676890811a825bc135425e8ac5f78180
Hernán <hfvaldivieso@uc.cl>
2021-11-10 16:55:14 -0300

    Prepare 0.0.4 release

--- PyNetworkD3/__init__.py
+++ PyNetworkD3/__init__.py
@@ -4,5 +4,5 @@ Init file for the PyNetworkD3 library.
 
 from PyNetworkD3.core import ArcDiagram, Graph
 
-version_info = (0, 0, 3)
+version_info = (0, 0, 4)
 __version__ = ".".join([str(x) for x in version_info])
