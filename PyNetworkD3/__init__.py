72730f5007adb091746c5b08d056b317d527a6e8
Hernán <hfvaldivieso@uc.cl>
2021-11-09 23:55:54 -0300

    Prepare 1.0.3 release

--- PyNetworkD3/__init__.py
+++ PyNetworkD3/__init__.py
@@ -4,5 +4,5 @@ Init file for the PyNetworkD3 library.
 
 from PyNetworkD3.core import Graph
 
-version_info = (1, 0, 2)
+version_info = (1, 0, 3)
 __version__ = ".".join([str(x) for x in version_info])
