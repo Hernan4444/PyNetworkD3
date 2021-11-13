f979c7ce05018c8bd6e8278b78099c70919378b1
Hernán <hfvaldivieso@uc.cl>
2021-11-13 13:07:45 -0300

    Prepare 0.0.6 release

--- PyNetworkD3/__init__.py
+++ PyNetworkD3/__init__.py
@@ -4,5 +4,5 @@ Init file for the PyNetworkD3 library.
 
 from PyNetworkD3.core import AdjacencyMatrix, ArcDiagram, ForceGraph
 
-version_info = (0, 0, 5)
+version_info = (0, 0, 6)
 __version__ = ".".join([str(x) for x in version_info])
